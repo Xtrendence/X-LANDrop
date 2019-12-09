fe6c1c58bfa51dd67630e64ff047d3b5a3efc3b2
Xtrendence <xtrendence@yahoo.com>
2019-12-09 01:19:20 +0000

    Update.

--- server.js
+++ server.js
@@ -56,10 +56,7 @@ app.post("/receive", function(req, res) {
 });
 
 app.get("/receive", function(req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.send("active");
 });
 
